4cc561c83cae5fc39aab6a1bd90476b1a890d6a5
Nikita Smirnov <kitos.smirnov@gmail.com>
2025-07-05 13:16:35 +0200

    init

---
+++ main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/gorilla/websocket"
+)
+
+type tradeMsg struct {
+	Price string `json:"p"`
+}
+
+func main() {
+	const wsURL = "wss://stream.binance.com:9443/ws/btcusdc@trade"
+
+	conn, _, err := websocket.DefaultDialer.Dial(wsURL, nil)
+	if err != nil {
+		log.Fatalf("dial error: %v", err)
+	}
+	defer conn.Close()
+
+	interrupt := make(chan os.Signal, 1)
+	signal.Notify(interrupt, os.Interrupt, syscall.SIGTERM)
+
+	done := make(chan struct{})
+
+	go func() {
+		defer close(done)
+		for {
+			_, msg, err := conn.ReadMessage()
+			if err != nil {
+				log.Printf("read error: %v", err)
+				return
+			}
+
+			var t tradeMsg
+			if err := json.Unmarshal(msg, &t); err != nil {
+				log.Printf("json error: %v", err)
+				continue
+			}
+
+			log.Printf("%s BTC/USDC price: %s", time.Now().Format(time.RFC3339), t.Price)
+		}
+	}()
+
+	for {
+		select {
+		case <-done:
+			return
+		case <-interrupt:
+			log.Println("interrupt")
+
+			_ = conn.WriteMessage(
+				websocket.CloseMessage,
+				websocket.FormatCloseMessage(
+					websocket.CloseNormalClosure, "",
+				),
+			)
+			return
+		}
+	}
+}
